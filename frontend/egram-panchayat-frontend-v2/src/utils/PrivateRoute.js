65abe5ecda7f8232e7682a226120a55f35b163d3
Harsh9370 <142889168+Harsh9370@users.noreply.github.com>
2026-02-11 12:00:40 +0530

    fix PrivateRoute component
    Removed comments regarding login status checks.

--- frontend/egram-panchayat-frontend-v2/src/utils/PrivateRoute.js
+++ frontend/egram-panchayat-frontend-v2/src/utils/PrivateRoute.js
@@ -3,12 +3,11 @@ import { Navigate } from "react-router-dom";
 function PrivateRoute({ children }) {
   const token = localStorage.getItem("token");
 
-  // 🔐 Not logged in → redirect to login
+  
   if (!token) {
     return <Navigate to="/" replace />;
   }
 
-  // ✅ Logged in → allow access
   return children;
 }
 
